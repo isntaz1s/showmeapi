54a40a7dfabd9eec3374ab45a999b4ca181674d4
isntaz1s <azis190407@gmail.com>
2024-11-12 23:29:29 +0700

    chore(schemas): changed zod max length of poems schema

--- src/schemas/poems.schema.ts
+++ src/schemas/poems.schema.ts
@@ -3,15 +3,15 @@ import { z } from 'zod';
 // create poem schema for request body
 export const createPoemSchema = z
   .object({
-    body: z.string().min(16).max(100),
-    author: z.string().includes('@').min(3).max(16),
+    body: z.string().min(16).max(255),
+    author: z.string().includes('@').min(3).max(100),
   })
   .required();
 
 // update poem schema for request body
 export const updatePoemSchema = z
   .object({
-    body: z.string().min(16).max(100),
-    author: z.string().includes('@').min(3).max(16),
+    body: z.string().min(16).max(255),
+    author: z.string().includes('@').min(3).max(100),
   })
   .optional();
