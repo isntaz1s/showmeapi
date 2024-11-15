4247e48a2facbc45b8cfa7e8b3f10cec22bb35f2
isntaz1s <azis190407@gmail.com>
2024-11-15 10:24:20 +0700

    ops(main): fastify serverless emit

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import app from '~/app';
 import { env } from './config/env';
+import { FastifyRequest, FastifyReply } from 'fastify';
 
 // initialize main application
-async function main() {
-  await app.listen({ port: env.PORT }, (err) => {
+export default async (request: FastifyRequest, reply: FastifyReply) => {
+  await app.ready();
+  app.server.emit('request', request, reply);
+  app.listen({ port: env.PORT }, (err) => {
     if (err) {
       app.log.info(`Server could not listening, because ${err}`);
     }
   });
   return app;
 }
-
-main();
